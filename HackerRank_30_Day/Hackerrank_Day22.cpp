6b4de5b2dd8099d35646fc305e541354e2938b83
mtemel <mmtemel41@gmail.com>
2023-07-01 18:12:01 +0300

    day22(binary search tree)

---
+++ HackerRank_30_Day/Hackerrank_Day22.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+        int h = 0;
+		int getHeight(Node* root){
+            //Write your code here
+            if(!root)
+                return 0;
+            else if(getHeight(root->right) == 0 && getHeight(root->left) == 0)
+                return 1;
+            h = max(h + getHeight(root->right), h + getHeight(root->left));
+            return h;
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
